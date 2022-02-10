fdfc8af75ee522f60a813b95409f9eba745ef91b
natea123 <andrewsnate94@gmail.com>
2022-02-10 14:22:52 -0500

    init

---
+++ PaymentAPI/Models/PaymentDetailContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace PaymentAPI.Models
+{
+    public class PaymentDetailContext:DbContext
+    {
+        public PaymentDetailContext(DbContextOptions<PaymentDetailContext> options):base(options)
+        {
+
+        }
+
+        public DbSet<PaymentDetail> PaymentDetails { get; set; }
+        
+    }
+}
